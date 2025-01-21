fd69b48ef32d587cd5747ce7cd3804b83a5edcd6
Анастасия <Angelice92@mail.ru>
2025-01-21 21:19:11 +0300

    start new angular 19.0 table with PrimeNg

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {RouterOutlet} from '@angular/router';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    RouterOutlet
+  ]
+})
+
+export class AppModule {
+}
